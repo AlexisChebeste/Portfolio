09a543e5cc76f9bb368b0bedbad8221a705ad31e
AlexisChebeste <alexispaulino.chebeste@estudiantes.unahur.edu.ar>
2024-10-14 01:27:05 -0300

    themeToggle

--- src/Icon/index.js
+++
@@ -1,8 +0,0 @@
-// Icon/index.js
-export { default as HtmlIcon } from './skills/Html.astro';
-export { default as CssIcon } from './skills/Css.astro';
-export { default as JavaScriptIcon } from './skills/JavaScript.astro';
-export { default as TypeScriptIcon } from './skills/TypeScript.astro';
-export { default as ReactIcon } from './skills/React.astro';
-export { default as AstroIcon } from './skills/Astro.astro';
-export { default as TailwindCssIcon } from './skills/TailwindCss.astro';
